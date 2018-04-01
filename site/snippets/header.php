07529f5c111bedb8ce9ee132257294bbca6deaab
michael <michael@helloprint.com>
2018-04-01 21:26:21 +0200

    updates

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -20,68 +20,13 @@
   			<div class="inner">
 
   				<div class="site-title">
-  					<h1><a href="index.html"><img src="assets/img/logo.png" width="144" height="52" alt="Exa"></a></h1>
+  					<h1><a href="<?= $site->home()->url() ?>"><img src="assets/img/logo.png" width="144" height="52" alt="Exa"></a></h1>
   					<a href="#site-menu" class="site-menu-toggle">
   						<span class="sr-only">Toggle navigation</span>
   						<em class="first"></em><em class="middle"></em><em class="last"></em>
   					</a>
   				</div>
-
-  				<div id="site-menu">
-  					<nav>
-  						<ul>
-  							<li class="current-menu-item"><a href="index.html">Home</a></li>
-  							<li><a href="about.html">About</a></li>
-  							<li class="menu-item-has-children">
-  								<a href="work.html">Work</a>
-  								<ul>
-  									<li><a href="work.html">Classic Gallery</a></li>
-  									<li><a href="work-categories.html">Category Based</a></li>
-  									<li><a href="project.html">Single Project</a></li>
-  								</ul>
-  							</li>
-  							<li class="menu-item-has-children">
-  								<a href="blog.html">Blog</a>
-  								<ul>
-  									<li><a href="blog.html"> Fullpage Blog</a></li>
-  									<li><a href="blog-sidebar.html">Blog with Sidebar</a></li>
-  									<li><a href="post.html">Single Blog Post</a></li>
-  								</ul>
-  							</li>
-  							<li class="menu-item-has-children">
-  								<a href="features.html">Features</a>
-  								<ul>
-  									<li><a href="features.html">Styleguide</a></li>
-  									<li><a href="clients.html">Clients</a></li>
-  									<li class="menu-item-has-children">
-  										<a href="#">Submenu Test</a>
-  										<ul>
-  											<li><a href="#">Submenu Item 1</a></li>
-  											<li><a href="#">Submenu Item 2</a></li>
-  											<li><a href="#">Submenu Item 3</a></li>
-  										</ul>
-  									</li>
-  									<li><a href="404.html">404 Not Found</a></li>
-  								</ul>
-  							</li>
-  							<li class="menu-item-has-children">
-  								<a href="contact1.html">Contact</a>
-  								<ul>
-  									<li><a href="contact1.html">Contact &mdash; Style 1</a></li>
-  									<li><a href="contact2.html">Contact &mdash; Style 2</a></li>
-  								</ul>
-  							</li>
-  							<li class="menu-item-search">
-  								<a href="search.html"><i class="fa fa-search"></i><i class="fa fa-times"></i></a>
-  								<form method="get" id="site-searchform" class="searchform" action="search.html">
-  									<input type="search" name="s" autocomplete="off" placeholder="Search&hellip;">
-  									<input type="submit" value="&#xf002;">
-  								</form>
-  							</li>
-  						</ul>
-  					</nav>
-  				</div>
-
+          <?php snippet('menu') ?>
   			</div>
   		</div>
   	</header>
--- site/snippets/menu.php
+++ site/snippets/menu.php
@@ -1,9 +1,11 @@
-<nav class="navigation column" role="navigation">
-  <ul class="menu">
-    <?php foreach($pages->visible() as $item): ?>
-    <li class="menu-item<?= r($item->isOpen(), ' is-active') ?>">
-      <a href="<?= $item->url() ?>"><?= $item->title()->html() ?></a>
-    </li>
-    <?php endforeach ?>
-  </ul>
-</nav>
+<div id="site-menu">
+  <nav>
+    <ul>
+      <?php foreach($pages->visible() as $item): ?>
+      <li class="menu-item<?= r($item->isOpen(), ' is-active') ?>">
+        <a href="<?= $item->url() ?>"><?= $item->title()->html() ?></a>
+      </li>
+      <?php endforeach ?>
+    </ul>
+  </nav>
+</div>
