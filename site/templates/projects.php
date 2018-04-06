804b49b40bf4fe2c38ddf369b8e94dfe2da76616
michael <michael@helloprint.com>
2018-04-06 22:58:40 +0200

    project page updates

--- site/templates/projects.php
+++ site/templates/projects.php
@@ -5,143 +5,73 @@
 
     <div class="margin-2"></div>
 
-    <h6>Branding</h6>
+    <h6>Restyling</h6>
     <div class="sep red margin-1"></div>
     <div class="owl-carousel owl-thumbs">
-      <div class="thumb dark-overlay">
-        <div class="photo">
-          <a href="project.html">
-            <img src="img/sample-thumb.png" alt="" width="434" height="434">
-            <span class="info"><span class="big-excerpt">Din Kook</span></span>
-          </a>
+      <?php
+        $filtered = $page->children()->filterBy('tags', 'restyling', '/');
+        foreach($filtered as $article): ?>
+        <div class="thumb dark-overlay">
+          <div class="photo">
+            <a href="<?= $article->url() ?>">
+              <?php
+              $image = $article->cover_image()->toFile();
+              if($image):
+              ?>
+              <img src="<?= $image->url() ?>" alt="" width="434" height="434">
+              <?php endif ?>
+              <span class="info"><span class="big-excerpt"><?= $article->title()->html() ?></span></span>
+            </a>
+          </div>
         </div>
-      </div>
-      <div class="thumb">
-        <div class="photo">
-          <a href="project.html">
-            <img src="img/sample-thumb.png" alt="" width="434" height="434">
-            <span class="info"><span class="big-excerpt">Suprette</span></span>
-          </a>
-        </div>
-      </div>
-      <div class="thumb dark-overlay">
-        <div class="photo">
-          <a href="project.html">
-            <img src="img/sample-thumb.png" alt="" width="434" height="434">
-            <span class="info"><span class="big-excerpt">Frock</span></span>
-          </a>
-        </div>
-      </div>
-      <div class="thumb">
-        <div class="photo">
-          <a href="project.html">
-            <img src="img/sample-thumb.png" alt="" width="434" height="434">
-            <span class="info"><span class="big-excerpt">Maon</span></span>
-          </a>
-        </div>
-      </div>
-      <div class="thumb dark-overlay">
-        <div class="photo">
-          <a href="project.html">
-            <img src="img/sample-thumb.png" alt="" width="715" height="715">
-            <span class="info"><span class="big-excerpt">Northbound</span></span>
-          </a>
-        </div>
-      </div>
+        <?php endforeach ?>
     </div>
     <div class="margin-4"></div>
 
-    <h6>UI/UX Design</h6>
+    <h6>Op maat</h6>
     <div class="sep red margin-1"></div>
     <div class="owl-carousel owl-thumbs">
-      <div class="thumb">
-        <div class="photo">
-          <a href="project.html">
-            <img src="img/sample-thumb.png" alt="" width="434" height="434">
-            <span class="info"><span class="big-excerpt">The Nordic</span></span>
-          </a>
+      <?php
+        $filtered = $page->children()->filterBy('tags', 'opmaat', '/');
+        foreach($filtered as $article): ?>
+        <div class="thumb dark-overlay">
+          <div class="photo">
+            <a href="<?= $article->url() ?>">
+              <?php
+              $image = $article->cover_image()->toFile();
+              if($image):
+              ?>
+              <img src="<?= $image->url() ?>" alt="" width="434" height="434">
+              <?php endif ?>
+              <span class="info"><span class="big-excerpt"><?= $article->title()->html() ?></span></span>
+            </a>
+          </div>
         </div>
-      </div>
-      <div class="thumb dark-overlay">
-        <div class="photo">
-          <a href="project.html">
-            <img src="img/sample-thumb.png" alt="" width="434" height="434">
-            <span class="info"><span class="big-excerpt">Geneviève Caron</span></span>
-          </a>
-        </div>
-      </div>
-      <div class="thumb">
-        <div class="photo">
-          <a href="project.html">
-            <img src="img/sample-thumb.png" alt="" width="434" height="434">
-            <span class="info"><span class="big-excerpt">Northbound</span></span>
-          </a>
-        </div>
-      </div>
-      <div class="thumb dark-overlay">
-        <div class="photo">
-          <a href="project.html">
-            <img src="img/sample-thumb.png" alt="" width="434" height="434">
-            <span class="info"><span class="big-excerpt">Hoop</span></span>
-          </a>
-        </div>
-      </div>
-      <div class="thumb">
-        <div class="photo">
-          <a href="project.html">
-            <img src="img/sample-thumb.png" alt="" width="715" height="715">
-            <span class="info"><span class="big-excerpt">Frock</span></span>
-          </a>
-        </div>
-      </div>
+        <?php endforeach ?>
     </div>
     <div class="margin-4"></div>
 
-    <h6>Marketing</h6>
+    <h6>Luxe</h6>
     <div class="sep red margin-1"></div>
     <div class="owl-carousel owl-thumbs">
-      <div class="thumb dark-overlay">
-        <div class="photo">
-          <a href="project.html">
-            <img src="img/sample-thumb.png" alt="" width="434" height="434">
-            <span class="info"><span class="big-excerpt">Row Food</span></span>
-          </a>
+      <?php
+        $filtered = $page->children()->filterBy('tags', 'luxe', '/');
+        foreach($filtered as $article): ?>
+        <div class="thumb dark-overlay">
+          <div class="photo">
+            <a href="<?= $article->url() ?>">
+              <?php
+              $image = $article->cover_image()->toFile();
+              if($image):
+              ?>
+              <img src="<?= $image->url() ?>" alt="" width="434" height="434">
+              <?php endif ?>
+              <span class="info"><span class="big-excerpt"><?= $article->title()->html() ?></span></span>
+            </a>
+          </div>
         </div>
-      </div>
-      <div class="thumb">
-        <div class="photo">
-          <a href="project.html">
-            <img src="img/sample-thumb.png" alt="" width="434" height="434">
-            <span class="info"><span class="big-excerpt">Frock</span></span>
-          </a>
-        </div>
-      </div>
-      <div class="thumb dark-overlay">
-        <div class="photo">
-          <a href="project.html">
-            <img src="img/sample-thumb.png" alt="" width="434" height="434">
-            <span class="info"><span class="big-excerpt">Hoop</span></span>
-          </a>
-        </div>
-      </div>
-      <div class="thumb">
-        <div class="photo">
-          <a href="project.html">
-            <img src="img/sample-thumb.png" alt="" width="434" height="434">
-            <span class="info"><span class="big-excerpt">Suprette</span></span>
-          </a>
-        </div>
-      </div>
-      <div class="thumb dark-overlay">
-        <div class="photo">
-          <a href="project.html">
-            <img src="img/sample-thumb.png" alt="" width="715" height="715">
-            <span class="info"><span class="big-excerpt">Din Kook</span></span>
-          </a>
-        </div>
-      </div>
+        <?php endforeach ?>
     </div>
-
     <div class="margin-6"></div>
 
   </div>
