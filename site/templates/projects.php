915b67ea9b0278b89a355c1c77e66e59ba24a3fd
michael <michael@helloprint.com>
2018-04-10 22:47:47 +0200

    rename labels

--- site/templates/projects.php
+++ site/templates/projects.php
@@ -72,6 +72,29 @@
         </div>
         <?php endforeach ?>
     </div>
+    <div class="margin-4"></div>
+
+    <h6>Verkoopstyling</h6>
+    <div class="sep red margin-1"></div>
+    <div class="owl-carousel owl-thumbs">
+      <?php
+        $filtered = $page->children()->filterBy('tags', 'Woonstijladvies Verkoopstyling', '/');
+        foreach($filtered as $article): ?>
+        <div class="thumb dark-overlay">
+          <div class="photo">
+            <a href="<?= $article->url() ?>">
+              <?php
+              $image = $article->cover_image()->toFile();
+              if($image):
+              ?>
+              <img src="<?= $image->url() ?>" alt="" width="434" height="434">
+              <?php endif ?>
+              <span class="info"><span class="big-excerpt"><?= $article->title()->html() ?></span></span>
+            </a>
+          </div>
+        </div>
+        <?php endforeach ?>
+    </div>
     <div class="margin-6"></div>
 
   </div>
