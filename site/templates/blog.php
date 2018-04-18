8139d5b5a886df7d000923023f3e7fbd2cc33ad8
michael <michael@helloprint.com>
2018-04-18 22:43:58 +0200

    fix blog

--- site/templates/blog.php
+++ site/templates/blog.php
@@ -9,56 +9,65 @@
           <div class="thumb featured">
             <div class="photo">
               <a href="post.html">
-                <img src="img/sample-thumb.png" alt="" width="715" height="715">
-                <span class="info"><strong class="big-title">Dominance, focal points and hierarchy</strong><span class="excerpt">Your content might be the king, but are you presenting it as one? Do people look where they should?</span><em class="arrow-right"></em></span>
+                <img src="assets/img/sample-thumb.png" alt="" width="715" height="715">
+                <span class="info"><strong class="big-title"><?= $page->header()->html() ?></strong><span class="excerpt"><?= $page->text()->html() ?></span><em class="arrow-right"></em></span>
               </a>
             </div>
           </div>
         </div>
 
-        <div class="box col-sm-5 col-md-3">
-          <div class="post-box vertical clearfix">
-            <a href="post.html" class="image-link arrow-icon"><img src="img/sample-thumb.png" width="434" height="434" alt=""></a>
-            <div class="extra-info">
-              <p class="meta small">1 day ago by <a href="#">Nicole</a></p>
-              <h5><a href="post.html">Harnessing Flexbox For Today’s Webapps</a></h5>
+        <?php
+        $adviseItems = $page->advise()->toStructure();
+        $ind = 0;
+        foreach($adviseItems as $adviseItem):
+          // try to get an image object from the filename
+          $image = $adviseItem->image()->toFile();
+          ?>
+          <?if($ind===0): ?>
+            <div class="box col-sm-5 col-md-3">
+              <div class="post-box vertical clearfix">
+                <a href="<?= $adviseItem->link()->html() ?>" class="image-link arrow-icon">
+                  <?if($image): ?>
+                    <img src="<?= $image->url() ?>" width="434" height="434" alt="">
+                  <?php endif ?>
+                </a>
+                <div class="extra-info">
+                  <p class="meta small">1 day ago by <a href="#">Nicole</a></p>
+                  <h5><a href="<?= $adviseItem->link()->html() ?>"><?= $adviseItem->title()->html() ?></a></h5>
+                </div>
+              </div>
             </div>
-          </div>
-        </div>
-
-        <div class="box col-sm-6 col-md-3">
-          <div class="post-box vertical clearfix">
-            <div class="extra-info">
-              <p class="meta small">2 day ago by <a href="#">Sheila</a></p>
-              <h5><a href="post.html">Conducting User Research in other countries</a></h5>
+          <?elseif($ind==1): ?>
+            <div class="box col-sm-6 col-md-3">
+              <div class="post-box vertical clearfix">
+                <div class="extra-info">
+                  <p class="meta small">2 day ago by <a href="#">Sheila</a></p>
+                  <h5><a href="<?= $adviseItem->link()->html() ?>"><?= $adviseItem->title()->html() ?></a></h5>
+                </div>
+                <a href="<?= $adviseItem->link()->html() ?>" class="image-link arrow-icon dark-overlay">
+                  <?if($image): ?>
+                    <img src="<?= $image->url() ?>" width="434" height="434" alt="">
+                  <?php endif ?>
+                </a>
+              </div>
             </div>
-            <a href="post.html" class="image-link arrow-icon dark-overlay"><img src="img/sample-thumb.png" width="434" height="434" alt=""></a>
-          </div>
-        </div>
-
-        <div class="box col-sm-6">
-          <div class="post-box horizontal clearfix">
-            <a href="post.html" class="image-link play-icon dark-overlay"><img src="img/sample-thumb.png" width="434" height="434" alt=""></a>
-            <div class="extra-info">
-              <p class="meta small">4 days ago by <a href="#">Andrew</a></p>
-              <h5><a href="post.html">Putting Mobile Backend as a service into practice</a></h5>
+          <?else: ?>
+            <div class="box col-sm-6">
+              <div class="post-box horizontal clearfix">
+                <a href="<?= $image->url() ?>" class="image-link arrow-icon">
+                  <?if($image): ?>
+                    <img src="<?= $image->url() ?>" width="434" height="434" alt="">
+                  <?php endif ?>
+                </a>
+                <div class="extra-info">
+                  <p class="meta small">5 days ago by <a href="#">Sheila</a></p>
+                  <h5><a href="<?= $image->url() ?>"><?= $adviseItem->link()->html() ?></a></h5>
+                </div>
+              </div>
             </div>
-          </div>
-        </div>
-
-        <div class="box col-sm-6">
-          <div class="post-box horizontal clearfix">
-            <a href="post.html" class="image-link arrow-icon"><img src="img/sample-thumb.png" width="434" height="434" alt=""></a>
-            <div class="extra-info">
-              <p class="meta small">5 days ago by <a href="#">Sheila</a></p>
-              <h5><a href="post.html">Product Design Unification Study: Mobile Web Framework</a></h5>
-            </div>
-          </div>
-        </div>
-
-        <div class="box col-sm-12 text-center">
-          <a href="blog.html" class="button color no-bottom">Load More Posts</a>
-        </div>
+          <?php endif ?>
+          <?$ind++?>
+        <?php endforeach ?>
 
       </div>
     </div>
