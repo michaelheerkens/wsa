834867441cb23a0686dd13b7bab8a85634f05c04
michael <michael@helloprint.com>
2018-05-21 22:13:59 +0200

    updates

--- site/templates/about.php
+++ site/templates/about.php
@@ -12,7 +12,7 @@
       <div class="margin-4"></div>
       <div class="row">
 				<div class="col-md-6">
-					<h4><?= $page->intro()->html() ?></h4>
+					<h3><?= $page->intro()->html() ?></h3>
 					<div class="margin-4"></div>
 				</div>
 				<div class="col-md-6">
