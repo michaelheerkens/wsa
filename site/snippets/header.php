05a0e24ce800c6efb44de74492f3e51fed2e9749
michael <michael@helloprint.com>
2018-05-24 12:17:43 +0200

    added google

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -12,6 +12,19 @@
   	<link href="https://fonts.googleapis.com/css?family=Playfair+Display:400,400italic%7CRoboto:400,700%7CDroid+Serif" rel="stylesheet" type="text/css">
     <?= css('assets/css/font-awesome.min.css') ?>
     <?= css('assets/css/style.css') ?>
+    <script type="text/javascript">
+
+    var _gaq = _gaq || [];
+    _gaq.push(['_setAccount', 'UA-27977332-1']);
+    _gaq.push(['_trackPageview']);
+
+    (function() {
+      var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+      ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+      var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+    })();
+
+  </script>
   </head>
   <body>
 
