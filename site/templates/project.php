909b17f81b2c14817270dcb65b61b4d5962f6ee1
michael <michael@helloprint.com>
2018-05-27 15:06:49 +0200

    added project info

--- site/templates/project.php
+++ site/templates/project.php
@@ -17,6 +17,25 @@
       <?php endforeach ?>
     </div>
     <div class="clear"></div>
+    <div class="margin-3"></div>
+    <div class="row">
+      <div class="col-sm-4 col-md-3 project-properties">
+        <h6>Type woning</h6>
+        <p class="small"><?= $page->project()->html() ?></p>
+        <h6>Soort advies</h6>
+        <p class="small"><?= $page->title()->html() ?></p>
+        <h6>Woonstijl</h6>
+        <p class="small"><?= $page->style()->html() ?></p>
+        <p class="social-share">
+          <a href="https://twitter.com/woonstijladvies" class="social-link"><i class="fa fa-twitter"></i></a>
+          <a href="https://www.facebook.com/pages/Woonstijladvies/304331819608845" class="social-link"><i class="fa fa-facebook"></i></a>
+          <a href="https://www.instagram.com/woonstijladvies/" class="social-link"><i class="fa fa-instagram"></i></a>
+        </p>
+      </div>
+      <div class="col-sm-8 col-md-8 col-md-offset-1">
+        <?= $page->text()->kirbytext() ?>
+      </div>
+    </div>
     <div class="margin-4"></div>
     <div class="margin-6"></div>
   </div>
