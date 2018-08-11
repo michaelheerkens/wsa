6f3a1e174d28bcd98174d31ea4df71c654994383
michael <michael@helloprint.com>
2018-08-11 21:40:27 +0200

    changed h1

--- site/templates/article.php
+++ site/templates/article.php
@@ -4,7 +4,7 @@
   <div class="container">
     <div class="row">
       <div class="col-sm-10 col-sm-offset-1 col-md-8 col-md-offset-2">
-        <h2 class="single-title no-bottom"><?= $page->title()->html() ?></h2>
+        <h1 class="single-title no-bottom"><?= $page->title()->html() ?></h1>
         <p class="post-meta"><span>door <a href="#">Nicole</a></span></p>
         <p><?= $page->text()->kirbytext() ?></p>
       </div>
