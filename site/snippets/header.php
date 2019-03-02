65c4c54e28aca16f651d0001676194c74dbfdb62
michaelheerkens <michael.heerkens@gmail.com>
2019-03-02 16:20:42 +0100

    Update header.php

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -43,7 +43,7 @@ window.cookieconsent.initialise({
   "position": "top",
   "static": true,
   "content": {
-    "message": "Om je beter en persoonlijker te helpen, gebruiken wij cookies en vergelijkbare technieken. Met de cookies volgen wij en derde partijen jouw internetgedrag binnen onze site.  Als je verdergaat op onze website gaan we ervan uit dat je dat goedvindt. Meer weten? ",
+    "message": "Om je beter en persoonlijker te helpen, gebruiken wij cookies en vergelijkbare technieken. Met de cookies volgen wij en derde partijen jouw internetgedrag binnen onze site.  Als je verdergaat op onze website gaan we ervan uit dat je daarmee akkoord gaat. Meer weten? ",
     "dismiss": "OK!",
     "link": "Bekijk onze cookiepagina"
   }
